b08c1b0da5beaef8066d471629ff6c358f3126fb
unitiaathira <91459965+unitiaathira@users.noreply.github.com>
2021-09-29 10:37:12 +0530

    Update models.py

--- ecommerce/ecommerceproject/shoppingapp/models.py
+++ ecommerce/ecommerceproject/shoppingapp/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='category', blank=True)
+    image = models.ImageField(blank=True)
 
     class Meta:
         ordering = ('name',)
@@ -27,7 +27,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product', blank=True)
+    image = models.ImageField(blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
